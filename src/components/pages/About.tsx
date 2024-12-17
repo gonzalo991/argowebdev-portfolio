fc2763c63c841d29dd0a0e82f7d3955ed295cc01
Gonzalo Araya <gonxaraya991@gmail.com>
2024-12-16 21:57:20 -0300

    cambios finales

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -5,8 +5,10 @@ const About: React.FC = () => {
         <>
             <figure className="text-center mt-1 mb-3">
                 <figcaption className="blockquote-footer ms-5 me-5 mt-4 mb-3">
-                    Destaco por mis habilidades para comunicar ideas de manera clara y concisa, 
-                    mi enfoque empático me permite comprender las perspectivas de mis compañeros y buscar soluciones conjuntas. Siempre proactivo, busco oportunidades para mejorar el trabajo en equipo.
+                Me destaco por mi capacidad para comunicar ideas de forma clara y efectiva, con un enfoque 
+                empático que facilita la colaboración y la resolución conjunta de desafíos. Soy proactivo, 
+                autónomo y busco constantemente oportunidades para optimizar el trabajo en equipo y aportar 
+                soluciones de manera independiente cuando es necesario.
                 </figcaption>
             </figure>
         </>
