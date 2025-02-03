2c21608121306f3edece5de89c314b4e064d34e7
Gonzalo Araya <gonxaraya991@gmail.com>
2025-02-02 21:22:26 -0300

    se agrego el logo de fast api

--- src/components/fragments/Programming.tsx
+++ src/components/fragments/Programming.tsx
@@ -13,6 +13,7 @@ const Programming: React.FC = () => {
     const mysqlLogo = "https://www.logo.wine/a/logo/MySQL/MySQL-Logo.wine.svg";
     const mongodbLogo = "https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Picture.png";
     const expoLogo = "https://play-lh.googleusercontent.com/algsmuhitlyCU_Yy3IU7-7KYIhCBwx5UJG4Bln-hygBjjlUVCiGo1y8W5JNqYm9WW3s=w240-h480-rw";
+    const fastApiLogo = "https://imgs.search.brave.com/gEiAq38ZjKX2BSxywJvwP2CXk9jt9FzsjoM4D3SFEX8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9maWxl/cy5zdmdjZG4uaW8v/bG9nb3MvZmFzdGFw/aS5zdmc";
 
     return (
         <>
@@ -22,6 +23,7 @@ const Programming: React.FC = () => {
                 <div className="text-center mt-3 mb-3">
                     <img className="programming-icon" src={nestLogoUrl} alt="logo de nest js" />
                     <img className="programming-icon" src={flask} alt="logo de flask" />
+                    <img className="programming-icon" src={fastApiLogo} alt="logo de fast api" />
                     <img className="programming-icon" src={express} alt="logo de express js" />
                     <img className="programming-icon" src={next} alt="logo de next js" />
                     <img className="programming-icon" src={expoLogo} alt="logo de expo" />
